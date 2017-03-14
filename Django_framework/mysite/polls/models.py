7559da8ff0079947702b3ca5fe96ab393f940fe8
flyingbest <taeyoon4715@gmail.com>
2017-03-14 20:44:31 +0900

    django basic project app

---
+++ Django_framework/mysite/polls/models.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals
+from django.db import models
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+
+class Question(models.Model):
+	question_text = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published')
+	
+	def __unicode__(self):	# __str__ on Python3
+		return self.question_text
+
+	def was_published_recently(self):
+		now = timezone.now()
+		#return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+		return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+class Choice(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	choice_text = models.CharField(max_length=200)
+	votes = models.IntegerField(default=0)
+
+	def __unicode__(self):	# __str__ on Python3
+		return self.choice_text
